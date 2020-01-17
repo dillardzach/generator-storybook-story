b25339211c4c44cc17fb22fdbef9feb80a31234f
779g <rdp@779.mx>
2020-01-17 01:05:45 +0000

    added query to the story

--- generators/app/templates/story.js
+++ generators/app/templates/story.js
@@ -3,13 +3,18 @@ import React from 'react'
 //import { action } from '@storybook/addon-actions'
 
 import { <%= name %> } from 'ui'
+//import QUERY from './graphql/query.graphql'
+//import TestClient from 'stories/TestClient'
+
+//const endpoint = 'https://api.fwrlines.com/graphql'
 
 export default {
   title: '<%= fullname %>',
   component:<%= name %>,
   parameters: {
     decorators: [ 
-      //storyfn => <div className="">{ storyfn }</div>,
+      //storyfn => <div className="">{ storyfn() }</div>,
+      //storyfn => <TestClient endpoint={ endpoint }>{ storyfn() }</TestClient>,
     ]
   }
 }
