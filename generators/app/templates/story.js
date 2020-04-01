616fb0dfd34c1cdf5f4e7d00e661ddb1e18ff703
779g <rdp@779.mx>
2020-04-01 13:57:00 +0000

    Added list to dummy presets

--- generators/app/templates/story.js
+++ generators/app/templates/story.js
@@ -8,7 +8,7 @@ import { <%= name %> } from 'ui'
 //import { AplProvider } from 'stories/utils'
 //import { Router } from 'stories/utils'
 //import {ALL_COLORS, SIZES } from '../config.js'
-//import { TEXT_XXS_ESC, TEXT_XXS, TEXT_XS, TEXT } from '../utils/Dummy'
+//import { LIST, TEXT_XXS_ESC, TEXT_XXS, TEXT_XS, TEXT } from '../utils/Dummy'
 
 //const endpoint = 'https://api.fwrlines.com/graphql'
 
